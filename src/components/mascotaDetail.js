cb8333ccd1dd723d4cb2e944c365b019c122d39a
Juan Sebastian Pinzon <Js.pinzonr@uniandes.edu.co>
2022-11-15 10:32:21 -0500

    Version corregida (la inicial presentó un bug que me percaté luego de pushear).

--- src/components/mascotaDetail.js
+++ src/components/mascotaDetail.js
@@ -2,30 +2,16 @@ import { useParams } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
-const { useEffect, useState } = require("react");
-
-
-export default function Detail() {
-    useEffect(() => {
-      const URL =
-        "https://gist.githubusercontent.com/josejbocanegra/829a853c6c68880477697acd0490cecc/raw/99c31372b4d419a855e53f0e891246f313a71b20/mascotas.json";
-      fetch(URL)
-        .then((data) => data.json())
-        .then((data) => {
-          setMascotas(data);
-        });
-    }, []);
+import "./mascota.css";
+export default function Detail(props) {
+ const params = useParams();
    
-    console.log(params.mascotaId);
-    const params = useParams();
-    const [mascotas, setMascotas] = useState([]);
-    var mascota = mascotas.find(x => x.id === Number(params.mascotaId));
-    console.log(mascotas[0].id);
-    console.log(params.mascotaId);
-    console.log(mascota);
-    return (
-        <div style={{width:"50%", margin:"auto"}}>
+ const mascotas = props.mascotas;
+ const mascota = mascotas.find(x => x.id === Number(params.mascotaId));
+
+ console.log(mascota);
+ return (
+<div style={{width:"50%", margin:"auto"}}>
             <h1>I am {mascota.nombre}</h1>
             <div className="detail_container">
             <Card className="card_mascota">
@@ -49,5 +35,5 @@ export default function Detail() {
             </Card>
             </div>
         </div>
-    );
+ );
 }
--- src/components/mascotas.js
+++ src/components/mascotas.js
@@ -1,20 +1,11 @@
 import Mascota from "./mascota";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import React from 'react';
 
-const { useEffect, useState } = require("react");
-
-function Mascotas() {
- const [mascotas, setMascotas] = useState([]);
- useEffect(() => {
-   const URL =
-     "https://gist.githubusercontent.com/josejbocanegra/829a853c6c68880477697acd0490cecc/raw/99c31372b4d419a855e53f0e891246f313a71b20/mascotas.json";
-   fetch(URL)
-     .then((data) => data.json())
-     .then((data) => {
-       setMascotas(data);
-     });
- }, []);
+function Mascotas(props) {
+const mascotas = props.mascotas;
+ 
 
  return (
    <div className="container">
